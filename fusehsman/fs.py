135c2b2e8f6c0a3c344f1ff428555d93c406e09a
Nick Balih <nick.fyre@gmail.com>
2018-06-16 20:29:29 +0300

    Should work

--- fusehsman/fs.py
+++ fusehsman/fs.py
@@ -21,22 +21,22 @@ class Hierarchical(LoggingMixIn, Operations):
         self.archive_name = p.join(p.dirname(p.realpath(data)), 'archive.tar.gz')
         self.archive = tarfile.open(self.archive_name, mode='w:gz')
         self.archive.close()
-        self.archfhs = {}
+        self.archved_files = {}
         self.timer = {}
         self.rwlock = Lock()
 
     def compress(self, path, fh):
-        if fh in self.archfhs.keys() and self.archfhs[fh] == False:
+        if fh in self.archved_files.keys() and self.archved_files[fh] == False:
             with tarfile.open(self.archive_name, mode='w:gz') as tar:
                 tar.add(path)
             try:
                 os.remove(path)
             except OSError:
                 pass
-            self.archfhs[fh] = True
+            self.archved_files[fh] = True
 
     def extract(self, path, fh):
-        if fh in self.archfhs.keys() and self.archfhs[fh] == True:
+        if fh in self.archved_files.keys() and self.archved_files[fh] == True:
             old_archive_name = p.join(p.dirname(self.archive_name), 'old_archive.tar.gz')
             os.rename(self.archive_name, old_archive_name)
             original = tarfile.open(old_archive_name)
@@ -51,21 +51,21 @@ class Hierarchical(LoggingMixIn, Operations):
                 modified.addfile(info, extracted)
             original.close()
             modified.close()
-            self.timer[fh] = Timer(600.0, self.compress, [p.join(self.data, path), fh])
-            self.archfhs[fh] = False
+            self.timer[fh] = Timer(600.0, self.compress, [path, fh])
+            self.archved_files[fh] = False
         else:
             pass
 
-    # def __call__(self, op, path, *args):
-    #     return super(Hierarchical, self).__call__(op, p.join(self.data, path), *args)
+    def __call__(self, op, path, *args):
+        return super(Hierarchical, self).__call__(op, p.join(self.data, path), *args)
 
     chmod = os.chmod
     chown = os.chown
 
     def create(self, path, mode):
-        fh = os.open(p.join(self.data, path), os.O_WRONLY | os.O_CREAT | os.O_TRUNC, mode)
-        self.archfhs[fh] = False
-        self.timer[fh] = Timer(600.0, self.compress, [p.join(self.data, path), fh])
+        fh = os.open(path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, mode)
+        self.archved_files[fh] = False
+        # self.timer[fh] = Timer(600.0, self.compress, [path, fh])
         return fh
 
     def flush(self, path, fh):
@@ -78,7 +78,7 @@ class Hierarchical(LoggingMixIn, Operations):
           return os.fsync(fh)
 
     # def getattr(self, path, fh=None):
-    #     st = os.lstat(p.join(self.data, path))
+    #     st = os.lstat(path)
     #     return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
     #         'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 
@@ -92,9 +92,9 @@ class Hierarchical(LoggingMixIn, Operations):
     mknod = os.mknod
 
     def open(self, path, flags):
-        self.create(p.join(self.data, path + '.snapshot'), mode=None)
-        copyfile(p.join(self.data, path), p.join(self.data, path + '.snapshot'))
-        fh = os.open(p.join(self.data, path), flags)
+        # self.create(p.join(self.data, path + '.snapshot'), mode=None)
+        # copyfile(path, path + '.snapshot')
+        fh = os.open(path, flags)
         return fh
 
     def read(self, path, size, offset, fh):
@@ -105,7 +105,7 @@ class Hierarchical(LoggingMixIn, Operations):
     def readdir(self, path, fh):
         with tarfile.open(self.archive_name) as tar:
             tarlist = tar.list(verbose=False)
-        return ['.', '..'] + os.listdir(p.join(self.data, path)) + tarlist
+            return ['.', '..'] + os.listdir(path) + tarlist
 
     readlink = os.readlink
 
@@ -113,12 +113,12 @@ class Hierarchical(LoggingMixIn, Operations):
         return os.close(fh)
 
     def rename(self, old, new):
-        return os.rename(old, self.data + new)
+        return os.rename(old, p.join(self.data, new))
 
     rmdir = os.rmdir
 
     def statfs(self, path):
-        stv = os.statvfs(p.join(self.data, path))
+        stv = os.statvfs(path)
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
             'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
             'f_frsize', 'f_namemax'))
@@ -127,7 +127,7 @@ class Hierarchical(LoggingMixIn, Operations):
         return os.symlink(source, target)
 
     def truncate(self, path, length, fh=None):
-        with open(p.join(self.data, path), 'r+') as f:
+        with open(path, 'r+') as f:
             f.truncate(length)
 
     unlink = os.unlink
